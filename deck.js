becc4068a37cb8272bf3f166b0f2e44a58fd1233
Khris Kempis <khriskempis@gmail.com>
2019-03-27 00:12:18 -0700

    add deck functionality

--- deck.js
+++ deck.js
@@ -1,19 +1,55 @@
 class Deck {
   constructor(cards){
-    this.deck = cards
-    this.discardedCards = [];
+    // init with shuffled deck;
+    this.deck = this._shuffle(cards);
+    this.discardPile = [];
   }
 
   deal(num){
-
+    // make sure deck is init with card ids
     if(this.deck == undefined){
       throw new Error('Deck is empty; Make sure to pass cards to Deck constructor')
     }
+    // shuffle in discard pile when deck is 10 cards 
+    if(this.deck < 10){
+      this._shuffleDiscardPile();
+    }
     const cards = []
     for(let i=0; i<num; i++){
+      // deal out from beginning of deck 
       cards.push(this.deck.shift())
     }
+    console.log("cards dealt", cards);
     return cards;
+  }
+
+  discard(cards){
+    // check if cards is empty
+    if(cards.length == 0){
+      throw new Error("Discard pile is empty; No cards to discard");
+    }
+    // set discard pile to be the old and recently discarded pile
+    this.discardPile = [...this.discardPile, ...cards]
+    console.log("cards discarded", this.discardPile);
+  }
+
+  _shuffleDiscardPile(){
+    // shuffle discard pile
+    const shuffledCards = this._shuffle(this.discardPile);
+    // set deck to be the shuffled cards appended to original deck
+    this.deck = [...this.deck, ...shuffledCards]
+    console.log("cards shuffled", this.deck);
+  }
+
+  _shuffle(arr){
+    // Knuth shuffle algorithm
+    for(let i=arr.length -1; i>0; i--){
+      let index = Math.floor(Math.random() * i);
+      let temp = arr[index];
+      arr[index] = arr[i];
+      arr[i] = temp; 
+    }
+    return arr; 
   }
 }
 
--- index.js
+++ index.js
@@ -2,27 +2,36 @@ const cardIndex = require('./cardIndex')
 const Deck = require('./deck')
 const CardMap = require('./hashMap');
 
-
+// local Hash map of cards
 const cardHashMap = new CardMap;
-//
+//creating the hashmap
 cardIndex.forEach(card => {
   cardHashMap.set(card.id, card)
 })
 
+// grabbing card ids from the index
 cardIds = cardIndex.map(card => card.id)
-
+// creating a new deck with Card Ids
 const myDeck = new Deck(cardIds)
+// console.log(cardIds)
 
+// testing deal function 
 let dealtCards = myDeck.deal(5);
+// console.log(dealtCards);
 
-//
 
-console.log(dealtCards);
-
+// testing finding cards from cardIndex and grabbing 
+// specific info from data
 let cards = []
 
 dealtCards.forEach(cardId => {
-  cards.push(cardHashMap.get(cardId).produce);
+  cards.push(cardHashMap.get(cardId));
 })
 
-console.log(cards);
+// discarding cards into discard pile
+myDeck.discard(dealtCards);
+
+myDeck.deal(1);
+
+// console.log(myDeck.deck);
+
