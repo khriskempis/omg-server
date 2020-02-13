f0cddd93fc23396957f5e2572e469ce426cd92e3
Khris Kempis <khriskempis@gmail.com>
2020-02-12 19:31:08 -0800

    update Card

---
+++ Card/Card.js
@@ -0,0 +1,27 @@
+class Card {
+  constructor(){
+    this.id = id;
+    this.name = name;
+    this.g_name = g_name;
+  }
+  
+}
+
+//     id: 1,
+//     name: "Brick Manufacture",
+//     g_name: "Ziegelei",
+//     price: 2,
+//     vp: 2,
+//     sun: false,
+//     type: "C",
+//     resource: "C",
+//     required_resource: {
+//       h: null,
+//       c: 1,
+//       l: null,
+//       s: 3,
+//       w: null
+//     },
+//     produce: "Brick",
+//     value: 2,
+//     chain: ["Clay", "Coal"]
