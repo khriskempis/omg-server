0c9695d0d5fef9e666d47adee1cb4ef5f86d9e22
Khris Kempis <kkempis@centerfield.com>
2020-02-18 18:35:35 -0800

    add card methods, create good object

--- GameObjects/Card.js
+++ GameObjects/Card.js
@@ -1,27 +1,87 @@
 class Card {
-  constructor(){
+  constructor(data){
+
+    const { 
+      id, 
+      name, 
+      germanName, 
+      cost, 
+      vp, 
+      img, 
+      hasSun, 
+      type, 
+      requiredResources, 
+      produce, 
+      value,
+    } = data
+
+    const good = new Good(data.good);
+    const produce = new Good(data.produce);
+    const chain = data.chain.map(item => {
+      return new Good(item.name); 
+    })
+
     this.id = id;
     this.name = name;
-    this.g_name = g_name;
+    this.germanName = germanName;
+    this.cost = cost
+    this.vp = vp, 
+    this.img = img,
+    this.hasSun = hasSun,
+    this.type = type, 
+    this.requiredResources = requiredResources,
+    this.produce = produce,
+    this.value = value,
+    this.good = good,
+    this.chain = chain
+
   }
   
+  read(){
+    return {
+      id: this.id, 
+      name: this.name, 
+      germanName: this.germanName, 
+      cost: this.cost,
+      vp: this.vp,
+      img: this.img,
+      hasSun: this.hasSun,
+      type: this.type,
+      requiredResources: this.requiredResources,
+      produce: this.produce,
+      value: this.value,
+      good: this.good,
+      chain: this.chain
+    }
+  }
 }
 
-//     id: 1,
-//     name: "Brick Manufacture",
-//     g_name: "Ziegelei",
-//     price: 2,
-//     vp: 2,
-//     sun: false,
-//     type: "C",
-//     resource: "C",
-//     required_resource: {
-//       h: null,
-//       c: 1,
-//       l: null,
-//       s: 3,
-//       w: null
-//     },
-//     produce: "Brick",
-//     value: 2,
-//     chain: ["Clay", "Coal"]
+module.export = Card; 
+
+// “Card” : {
+// 	“id” : 23,
+// 	“name” : “Brick Manufacturer,
+// 	“germanName” : “Ziegelei”,
+// 	“cost” : 2,
+// 	“vp” : 2,
+// 	“img” : “url”
+// 	“hasSun” : false,
+// 	“type” : “red”
+// 	“good” : { “name” : “clay”,
+// 		   “type” : “resource” },
+// 	“requiredResources” : [
+// { “resource” : “hay”, 
+//   “amount” : 4 }
+// { “resource” : “clay”,
+//   “amount” : 2
+// ],
+// 	“produce” : { “name” : “brick”,
+// 		        “type” : “produce” },
+// 	“value” : 2”,
+// 	“chain” : [
+// 		{ “name” : “clay”,
+//   “type” : “resource” },
+// { “name” : “coal”,
+//   “type” : “produce” }
+// 	],
+// }
